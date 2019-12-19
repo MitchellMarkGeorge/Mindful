3b3d0d6540513217254e1e69c345709daa5d35f7
MitchellMarkGeorge <mitchellmmarkgeorge@gmail.com>
2019-12-19 08:27:54 -0500

    worked on sentiment analysis

--- src/app.js
+++ src/app.js
@@ -1,2 +1,5 @@
 import * as content from './content';
 // if using background or popup script, import it here
+// might end up moving bundle.js, manifest and images to seperate folder and only deploy that
+// try and reduce filesize
+
--- src/content.js
+++ src/content.js
@@ -1,1 +1,37 @@
+import Sentiment from 'sentiment';
 console.log('hello')
+//console.log(document.activeElement)
+
+//document.body.onclick = 
+
+let activeElement;
+let sentiment = new Sentiment();
+let score = 0;
+let wrapperDiv;
+
+
+document.body.addEventListener('click', () => {
+    
+    activeElement = document.activeElement;
+    console.log(activeElement.contentEditable);
+// forms???
+    if (activeElement.tagName === 'INPUT' || activeElement.tagName === 'TEXTAREA' || activeElement.contentEditable === 'true') {
+        wrapperDiv = document.createElement('div');
+        activeElement.appendChild(wrapperDiv);
+        analyze();
+    }
+    // console.log(document.activeElement.tagName)
+    // console.log(sentiment.analyze(document.activeElement.value));
+    //might have to increase the scale to have more nuetral values (or devide by 10 like google nlp) 
+})
+
+function analyze() {
+
+    activeElement.addEventListener('input', () => {
+        //console.log(activeElement.value);
+        console.log(sentiment.analyze(activeElement.value));
+    })
+
+
+
+}
